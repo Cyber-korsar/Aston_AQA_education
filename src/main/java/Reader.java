9e137339b5f78a0c9560dbca0ae2eac70b15d3a0
Cyber_korsar <sergeykorsar@gmail.com>
2024-01-05 22:53:34 +0300

    Lesson_6 Done

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,17 +1,30 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Reader {
-    public static void main(String[] args) {
-        try (FileReader reader = new FileReader("File.csv")) {
-            // читаем посимвольно
-            int c;
-            while ((c = reader.read()) != -1) {
-                System.out.print((char) c);
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+    public static AppData readData() {
+        //чтение
+        try (Scanner scanner = new Scanner(new File("File.csv"))){
+        String header = scanner.nextLine();
+        String[] header_result = header.split(";");
+        ArrayList<String> data = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            data.add(scanner.nextLine());
         }
+        int[][] data_result = new int[data.size()][header_result.length];
+        for (int i = 0; i < data_result.length; i++) {
+            String[] data_string = data.get(i).split(";");
+            for (int j = 0; j < data_string.length; j++) {
+                data_result[i][j] = Integer.parseInt(data_string[j]);
+            }
+        }
+        scanner.close();
+        return new AppData(header_result, data_result);} catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }return null;
     }
 }
--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -2,16 +2,24 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Writer {
-    public static void main(String[] args) {
-        try (FileWriter writer = new FileWriter("File.csv", false)) {
-            // запись всей строки
-            String text = "Hello Gold!";
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('E');
-            writer.flush();
-        } catch (IOException ex) {
+    public static void writeData(AppData appData) {
+        try (FileWriter writer = new FileWriter("File.csv")){
+        for (int i = 0; i < appData.getHeader().length; i++) {
+            if (i <= appData.getHeader().length - 2) {
+                writer.write(appData.getHeader()[i] + ";");
+            } else
+                writer.write(appData.getHeader()[i] + "\n");
+        }
+        for (int i = 0; i < appData.getData().length; i++) {
+            for (int j = 0; j < appData.getData()[0].length; j++) {
+                if (j <= appData.getData()[0].length - 2) {
+                    writer.write(appData.getData()[i][j] + ";");
+                } else {
+                    writer.write(appData.getData()[i][j] + "\n");
+                }
+            }
+        }
+        writer.close();} catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
