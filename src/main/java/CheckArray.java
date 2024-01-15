620601e85d72c976c9da7be2085e19e303d91fc7
Cyber_korsar <sergeykorsar@gmail.com>
2024-01-16 01:26:59 +0300

    Lesson_7 formatting code

--- src/main/java/CheckArray.java
+++ src/main/java/CheckArray.java
@@ -1,28 +1,32 @@
 public class CheckArray {
-    private static int arrayLineSize = 4;
-    private static int arrayRowsSize = 4;
+    private static final int arrayLineSize = 4;
+    private static final int arrayColumnsSize = 4;
 
-    public static void transformToInt(String[][] array) {
+    public static void checkSizeArrayAndSumArray(String[][] array) {
         try {
-            if (array.length == arrayLineSize) {
-                for (int i = 0; i < arrayRowsSize; i++) {
-                    if (array[i].length != arrayRowsSize) throw new MyArraySizeException();
-                }
+            int i = 0;
+            if (array.length == arrayLineSize && array[i].length == arrayColumnsSize) {
+                System.out.println("Массив верного размера");
             } else throw new MyArraySizeException();
-            int sum = 0;
-            for (int line = 0; line < 4; line++) {
-                for (int index = 0; index < 4; index++) {
-                    try {
-                        sum += Integer.parseInt(array[line][index]);
-                    } catch (NumberFormatException e) {
-                        throw new MyArrayDataException("В данном массиве: в строке: " + line + ", в ячейка с индексом: " + index + " лежит не число");
-                    }
-                }
-            }
+            int sum = getSum(array);
             System.out.println("Сумма всех чисел массива: " + sum);
         } catch (MyArraySizeException | MyArrayDataException e) {
             e.printStackTrace();
             System.out.println("Расчет не выполнен");
         }
+    }
+
+    private static int getSum(String[][] array) throws MyArrayDataException {
+        int sum = 0;
+        for (int line = 0; line < arrayLineSize; line++) {
+            for (int columns = 0; columns < arrayColumnsSize; columns++) {
+                try {
+                    sum += Integer.parseInt(array[line][columns]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("В данном массиве: в строке: " + line + ", в колонке: " + columns + " лежит не число");
+                }
+            }
+        }
+        return sum;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         String[][] array = new String[][]{
-                {"2", "1", "2", "3"},
-                {"6", "7", "1", "9"},
+                {"2", "а", "2", "3"},
+                {"1", "8", "1", "9"},
                 {"2", "4", "2", "7"},
-                {"8", "q", "6", "2"}
+                {"8", "2", "6", "2"}
         };
-        CheckArray.transformToInt(array);
+        CheckArray.checkSizeArrayAndSumArray(array);
     }
 }
