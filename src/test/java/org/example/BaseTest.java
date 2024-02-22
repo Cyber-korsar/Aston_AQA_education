23b295f019762d1babe72c88d36f10a539929633
cyber_korsar <sergeykorsar@gmail.com>
2024-02-23 00:15:59 +0300

    Lesson_17_Draft

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -4,10 +4,9 @@ import io.appium.java_client.android.AndroidDriver;
 import org.example.utils.Driver;
 
 public class BaseTest {
-   public AndroidDriver driver = new Driver().getDriver();
-//    @BeforeTest
-    public AndroidDriver init(){
+    public AndroidDriver driver = new Driver().getDriver();
+
+    public AndroidDriver init() {
         return new Driver().getDriver();
     }
-
 }
